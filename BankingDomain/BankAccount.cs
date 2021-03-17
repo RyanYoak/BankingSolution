ca6f9d712971151eb0dab42b13a4d648f5b93834
Ryan Yoak <ryan.yoak@gmail.com>
2021-03-17 09:59:11 -0400

    Add project files.

---
+++ BankingDomain/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BankingDomain
+{
+    public class BankAccount
+    {
+        private decimal _balance = 5000; // Class Variable or 'Field'
+
+        public BankAccount()
+        {
+        }
+
+        public decimal GetBalance()
+        {
+            return _balance;
+        }
+
+        public void Deposit(decimal amountToDeposit)
+        {
+            _balance += amountToDeposit;
+        }
+
+        public void Withdraw(decimal amountToWithdraw)
+        {
+            _balance -= amountToWithdraw;
+        }
+    }
+}
---
+++ BankingTests/BankAccountTests.cs
@@ -0,0 +1,61 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BankingTests
+{
+    public class BankAccountTests
+    {
+        BankAccount _account;
+        decimal _balance;
+
+        public BankAccountTests()
+        {
+            _account = new BankAccount();
+            _balance = _account.GetBalance();
+        }
+
+        [Fact]
+        public void NewAccountsHaveCorrectBalance()
+        {
+            // Then
+            Assert.Equal(5000M, _balance);
+        }
+
+        [Fact]
+        public void DepositsIncreaseTheBalance()
+        {
+            // Given
+            var amountToDeposit = 42M;
+
+            // When
+            _account.Deposit(amountToDeposit);
+
+            // Then
+            Assert.Equal(
+                _balance + amountToDeposit,
+                _account.GetBalance()
+            );
+        }
+
+        [Fact]
+        public void WithdrawsDecraseTheBalance()
+        {
+            // Given
+            var amountToWithdraw = 42M;
+
+            // When
+            _account.Withdraw(amountToWithdraw);
+
+            // Then
+            Assert.Equal(
+                _balance - amountToWithdraw,
+                _account.GetBalance()
+            );
+        }
+    }
+}
