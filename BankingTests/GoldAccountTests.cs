984b6fabcb05653d9fc02d666aa5b28561a9e761
Ryan Yoak <ryan.yoak@gmail.com>
2021-03-17 10:57:50 -0400

    Also not the best

---
+++ BankingTests/GoldAccountTests.cs
@@ -0,0 +1,32 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BankingTests
+{
+    public class GoldAccountTests
+    {
+        GoldAccount _account;
+        decimal _initialAmount;
+
+        public GoldAccountTests()
+        {
+            _account = new GoldAccount();
+            _initialAmount = _account.GetBalance();
+        }
+
+        [Theory]
+        [InlineData(100, 10)]
+        public void GoldAccountsGetBonusOnDeposit(decimal amountToDeposit, decimal expectedBonus)
+        {
+            _account.Deposit(amountToDeposit);
+            var expected = _initialAmount + amountToDeposit + expectedBonus;
+
+            Assert.Equal(expected, _account.GetBalance());
+        }
+    }
+}
