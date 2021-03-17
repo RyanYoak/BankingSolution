66c8943529af819336de611b78cd913ad2e3b13d
Ryan Yoak <ryan.yoak@gmail.com>
2021-03-17 10:33:06 -0400

    Bad solution

--- BankingDomain/BankAccount.cs
+++ BankingDomain/BankAccount.cs
@@ -2,9 +2,16 @@
 
 namespace BankingDomain
 {
+    public enum AccountType
+    {
+        Standard,
+        Gold
+    }
+
     public class BankAccount
     {
         private decimal _balance = 5000; // Class Variable or 'Field'
+        public AccountType AccountType = AccountType.Standard;
 
         public BankAccount()
         {
@@ -17,7 +24,12 @@ namespace BankingDomain
 
         public void Deposit(decimal amountToDeposit)
         {
-            _balance += amountToDeposit;
+            decimal bonus = 0;
+            if(AccountType == AccountType.Gold)
+            {
+                bonus = amountToDeposit * .1M;
+            }
+            _balance += amountToDeposit + bonus;
         }
 
         public void Withdraw(decimal amountToWithdraw)
---
+++ BankingTests/GoldCustomersTests.cs
@@ -0,0 +1,35 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BankingTests
+{
+    public class GoldCustomersTests
+    {
+        BankAccount account;
+        decimal openingBalance;
+
+        public GoldCustomersTests()
+        {
+            account = new BankAccount();
+            openingBalance = account.GetBalance();
+            account.AccountType = AccountType.Gold;
+        }
+
+        [Theory]
+        [InlineData(100, 10)]
+        public void GetABonusOnDeposits(decimal amountToDeposit, decimal expectedBonus)
+        {
+            // When
+            account.Deposit(amountToDeposit);
+
+            // Then
+            var expected = openingBalance + amountToDeposit + expectedBonus;
+            Assert.Equal(expected, account.GetBalance());
+        }
+    }
+}
